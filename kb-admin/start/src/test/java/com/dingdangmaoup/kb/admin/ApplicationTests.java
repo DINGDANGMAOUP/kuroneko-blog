91662d05916c26cc764bffd1f04fabd07c2ed47a
dingdangmaoup <dingdangmaoup@gmail.com>
2022-12-14 16:18:06 +0800

    fix(pom): update
    1,cola

--- kb-admin/start/src/test/java/com/dingdangmaoup/kb/admin/ApplicationTests.java
+++ kb-admin/start/src/test/java/com/dingdangmaoup/kb/admin/ApplicationTests.java
@@ -3,8 +3,10 @@ package com.dingdangmaoup.kb.admin;
 import com.alibaba.cola.catchlog.CatchLogAspect;
 import com.alibaba.cola.domain.ApplicationContextHelper;
 import com.alibaba.cola.domain.DomainFactory;
+import com.baomidou.dynamic.datasource.DynamicRoutingDataSource;
 import com.baomidou.mybatisplus.extension.toolkit.ChainWrappers;
 import java.time.LocalDate;
+import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,8 +20,14 @@ class ApplicationTests {
   @Autowired
   CatchLogAspect catchAndLog;
 
+      @Autowired
+    private DataSource dataSource;
+
+
+
   @Test
   void contextLoads() {
-
+       DynamicRoutingDataSource ds = (DynamicRoutingDataSource) dataSource;
+    System.out.println(ds.getDataSources().keySet());
   }
 }
